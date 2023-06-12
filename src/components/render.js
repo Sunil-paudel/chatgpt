70b1843f8292a1d7ddf5714914289997f42df290
sunil paudel <paudelsunil16@gmail.com>
2023-06-12 18:53:08 +1000

    first commit

---
+++ src/components/render.js
@@ -0,0 +1,4 @@
+{/* Render Html element to the DOM*/}
+{/*
+ReactDom.render(componentToRender, targetNode)
+*/ }
--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+{/* Render Html element to the DOM*/}
+{/*
+ReactDom.render(componentToRender, targetNode)
+*/ }
